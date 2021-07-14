2b8ad4bb5f6110ae558a3bea786b40fcf5f6ed9d
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-07-14 14:19:18 -0700

    style sidenav and add assets folder

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,11 +1,22 @@
 import React from "react";
+import CloudLogo from "../assets/image/cloud_logo.png";
+import ChartIcon from "../assets/svg/overview_icon.svg";
 
-function SideNav() {
+const SideNav = () => {
   return (
-    <div>
-      <h3>Side Nav</h3>
+    <div className="side-nav">
+      <div className="side-nav__header-container">
+        <img src={CloudLogo} alt="Logo" className="side-nav__logo" />
+        <h3 className="side-nav__header-text">Expense Tracker</h3>
+      </div>
+      <ul className="side-nav__items">
+        <li className="side-nav__item">
+          <img src={ChartIcon} alt="overview-icon" />
+          <p className="side-nav__item-text">Overview</p>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default SideNav;
