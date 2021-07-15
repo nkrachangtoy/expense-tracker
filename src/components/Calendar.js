517f23cf16d65184a2a65e07d514a281d6076268
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-07-14 21:31:59 -0700

    style weekly calendar and install chart.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,41 @@
 import React from "react";
 
 const Calendar = () => {
+  const today = new Date().toDateString();
+
   return (
-    <div>
-      <h3>Calendar</h3>
+    <div className="calendar">
+      <h3 className="calendar__current-date">{today}</h3>
+      <ul className="calendar__weekly">
+        <li className="calendar__item">
+          <p className="calendar__day">Sun</p>
+          <p className="calendar__date">11</p>
+        </li>
+        <li className="calendar__item">
+          <p className="calendar__day">Mon</p>
+          <p className="calendar__date">12</p>
+        </li>
+        <li className="calendar__item">
+          <p className="calendar__day">Tue</p>
+          <p className="calendar__date">13</p>
+        </li>
+        <li className="calendar__item">
+          <p className="calendar__day">Wed</p>
+          <p className="calendar__date">14</p>
+        </li>
+        <li className="calendar__item">
+          <p className="calendar__day">Thu</p>
+          <p className="calendar__date">15</p>
+        </li>
+        <li className="calendar__item">
+          <p className="calendar__day">Fri</p>
+          <p className="calendar__date">16</p>
+        </li>
+        <li className="calendar__item">
+          <p className="calendar__day">Sat</p>
+          <p className="calendar__date">17</p>
+        </li>
+      </ul>
     </div>
   );
 };
